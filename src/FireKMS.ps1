88a51629c36ecabfe85091219b699687279cb694
jbono <joan@bono.cat>
2018-02-16 20:15:56 +0100

    User-Friendly script. Yes, I said user-friendly and it's a PowerShell Script

--- src/FireKMS.ps1
+++ src/FireKMS.ps1
@@ -1,4 +1,4 @@
-﻿<#
+<#
 .DESCRIPTION
     Firewall rules for KMS Pico Windows Activation
 .REQUIRES
@@ -7,7 +7,7 @@
     Name: FireKMS.ps1
     Author: Joan Bono <@joan_bono>
     Tested on Powershell v5.1
-    Version: 1.0.0
+    Version: 1.0.1
 .EXAMPLE
     .\FireKMS.ps1
 #>
@@ -16,26 +16,66 @@
 $ErrorActionPreference = "SilentlyContinue"
 
 function WriteRules {
-    Write-Host "`n`t [!]" -ForegroundColor Yellow -NoNewLine
+    Write-Host "`n`t [!] " -ForegroundColor Yellow -NoNewLine
     Write-Host "Adding Firewall Rules for KMS Pico Windows Activation...`n"
 
-    Write-Host "`t [+]" -ForegroundColor Green -NoNewLine
+    Write-Host "`t [+] " -ForegroundColor Green -NoNewLine
     Write-Host "Adding Inbound TCP rule...`n"
-    New-NetFirewallRule -DisplayName "KMSPicoIN_TCP" -Direction Inbound -Action Allow -LocalAddress Any -LocalPort Any -RemoteAddress Any -RemotePort 1688 -Protocol TCP -Enabled True | Out-null
+    New-NetFirewallRule `
+        -DisplayName "KMSPicoIN_TCP" `
+        -Direction Inbound `
+        -Action Allow `
+        -LocalAddress Any `
+        -LocalPort Any `
+        -RemoteAddress Any `
+        -RemotePort 1688 `
+        -Protocol TCP `
+        -Enabled True `
+        | Out-null
 
-    Write-Host "`t [+]" -ForegroundColor Green -NoNewLine
+    Write-Host "`t [+] " -ForegroundColor Green -NoNewLine
     Write-Host "Adding Inbound UDP rule...`n"
-    New-NetFirewallRule -DisplayName "KMSPicoIN_UDP" -Direction Inbound  -Action Allow -LocalAddress Any -LocalPort Any -RemoteAddress Any -RemotePort 1688 -Protocol UDP -Enabled True | Out-null
+    New-NetFirewallRule `
+        -DisplayName "KMSPicoIN_UDP" `
+        -Direction Inbound  `
+        -Action Allow `
+        -LocalAddress Any `
+        -LocalPort Any `
+        -RemoteAddress Any `
+        -RemotePort 1688 `
+        -Protocol UDP `
+        -Enabled True `
+        | Out-null
 
-    Write-Host "`t [+]" -ForegroundColor Green -NoNewLine
+    Write-Host "`t [+] " -ForegroundColor Green -NoNewLine
     Write-Host "Adding Outbound TCP rule...`n"
-    New-NetFirewallRule -DisplayName "KMSPicoOUT_TCP" -Direction Outbound -Action Allow -LocalAddress Any -LocalPort Any -RemoteAddress Any -RemotePort 1688 -Protocol TCP -Enabled True | Out-null
+    New-NetFirewallRule `
+        -DisplayName "KMSPicoOUT_TCP" `
+        -Direction Outbound `
+        -Action Allow `
+        -LocalAddress Any `
+        -LocalPort Any `
+        -RemoteAddress Any `
+        -RemotePort 1688 `
+        -Protocol TCP `
+        -Enabled True `
+        | Out-null
 
-    Write-Host "`t [+]" -ForegroundColor Green -NoNewLine
+    Write-Host "`t [+] " -ForegroundColor Green -NoNewLine
     Write-Host "Adding Outbound UDP rule...`n"
-    New-NetFirewallRule  -DisplayName "KMSPicoOUT_UDP" -Direction Inbound -Action Allow -LocalAddress Any -LocalPort Any -RemoteAddress Any -RemotePort 1688 -Protocol UDP -Enabled True | Out-null
+    New-NetFirewallRule  `
+        -DisplayName "KMSPicoOUT_UDP" `
+        -Direction Inbound `
+        -Action Allow `
+        -LocalAddress Any `
+        -LocalPort Any `
+        -RemoteAddress Any `
+        -RemotePort 1688 `
+        -Protocol UDP `
+        -Enabled True `
+        | Out-null
 
-    Write-Host "`t [i]" -ForegroundColor Cyan -NoNewLine
+    Write-Host "`t [i] " -ForegroundColor Cyan -NoNewLine
     Write-Host "Done!`n"
 
 }
@@ -48,7 +88,7 @@ function Test-IsAdmin  {
     if ($IsAdmin){
         WriteRules
     } else {
-        Write-Host "`n`t [-]" -ForegroundColor Red -NoNewLine
+        Write-Host "`n`t [-] " -ForegroundColor Red -NoNewLine
         Write-Host "Run the script as Administrator!`n"
     }
 }
